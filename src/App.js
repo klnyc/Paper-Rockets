5027040e2407aa2d38c9911d6d2761e47e0b779f
Kenneth Lai <klnyc6@gmail.com>
2021-02-22 18:57:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.applink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <div className="card" style={{ color: "black" }}>This is a test card</div>
+        <button className="btn btn-primary">Test Button</button>
       </header>
     </div>
   );
