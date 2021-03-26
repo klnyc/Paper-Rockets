fea4819c41bfd7a11239a5c3454980f62426f3e8
Kenneth Lai <kennethlai.ny@gmail.com>
2021-03-26 14:45:24 -0400

    extended watchlist added reset acc

--- src/components/Account.js
+++ src/components/Account.js
@@ -9,11 +9,25 @@ export const Account = (props) => {
         .catch((error) => console.log(error));
     }
 
+    const reset = () => {
+        const newUser = {
+            userID: user.userID,
+            email: user.email,
+            balance: 100000,
+            portfolio: [],
+            orders: [],
+            watchlist: ["AAPL"]
+        }
+        window.firebase.firestore().collection("users").doc(user.userID).set(newUser)
+        .then(() => setUser(newUser))
+        .catch((error) => console.log(error));
+    }
+
     return (
         <div>
             <h5 className="p-4">{user.email}</h5>
             <button className="btn btn-secondary btn-sm mx-4" onClick={() => signOut()}><Link to="/">Sign Out</Link></button>
-            <button className="btn btn-danger btn-sm mx-2">Reset Account</button>
+            <button className="btn btn-danger btn-sm mx-2" onClick={() => reset()}><Link to="/">Reset Account</Link></button>
         </div>
     )
 }
